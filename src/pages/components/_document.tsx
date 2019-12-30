b481a6207e2d7d901445714f9fb35ed3bf3465ae
theBashShell <edohbruno@gmail.com>
2019-12-30 00:44:41 +0000

    project config

---
+++ src/pages/components/_document.tsx
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class RqstDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/css/normalize.css" />
+          <title>home</title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default RqstDocument;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+ 
+const index = () => {
+  return (
+    <div>
+      hello
+    </div>
+  );
+}
+
+export default index;
