7806b2d6079eb8ebb694604c1ef1cc8e52a2490b
theBashShell <edohbruno@gmail.com>
2020-01-11 10:09:04 +0000

    external flow working

--- src/components/external/External.jsx
+++ src/components/external/External.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Page from "../page/Page";
 import { message } from "antd";
 import LoginForm from "../login/LoginForm";
+import { firebase } from "../../util/firebase";
+import CONFIG from "../../util/constants";
 
-const callback = () => {
-  
-}
+const { auth } = firebase;
+
+const callback = email => {
+  auth()
+    .signInWithEmailLink(email, window.location.href)
+    .then(() => {
+      window.localStorage.removeItem(CONFIG.user);
+      console.log("successful");
+    })
+    .catch(err => message.error({ content: err.message, duration: 8 }));
+};
 
 const External = () => {
+  useEffect(() => {
+    message.warning({
+      content: "enter your email to login",
+      duration: 8
+    });
+  }, []);
   return (
     <Page>
-      {message.warning({
-        content: "enter your email to login",
-        duration: 8
-      })}
       <div className="centered high">
-        <LoginForm />
+        <LoginForm {...{ callback }} />
       </div>
     </Page>
   );
