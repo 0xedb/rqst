b14281c495e8284c64c2aa6473cb632752bf1263
theBashShell <edohbruno@gmail.com>
2020-01-12 00:16:17 +0000

    forming...

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import Page from '../page/Page'
+import React from "react";
+import Page from "../page/Page";
+import "./form.css";
+import { Form as AntForm, Input, InputNumber } from "antd";
+import { useFormik } from "formik";
 
 const Form = () => {
   return (
     <Page>
-      form here
+      <div className="rqst-form centered">
+        <form>
+          <AntForm.Item>
+            <Input type="text" placeholder="Name" size="large" />
+          </AntForm.Item>
+          <AntForm.Item>
+            <InputNumber min={0} placeholder="Event Budget" size="large" />
+          </AntForm.Item>
+          <AntForm.Item>
+            <InputNumber min={0} placeholder="Request Amount" size="large" />
+          </AntForm.Item>
+          <AntForm.Item>
+            <Input.TextArea placeholder="Funds Purpose" size="large" />
+          </AntForm.Item>
+          <AntForm.Item>
+            <Input
+              type="text"
+              placeholder="Club / Society / Group"
+              size="large"
+            />
+          </AntForm.Item>
+        </form>
+      </div>
     </Page>
   );
-}
+};
 
 export default Form;
