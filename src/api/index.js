10586a7b95d1b050469f97492dac68c7253e3d12
theBashShell <edohbruno@gmail.com>
2020-01-13 07:09:54 +0000

    setup for major test

--- src/api/index.js
+++ src/api/index.js
@@ -1,17 +1,9 @@
 const makePdf = require("../util/pdf");
 const sendMail = require("../util/email");
 
-module.exports = (req, res) => {
-  // let data = JSON.parse(req.body);
-  // let dd = JSON.parse(JSON.stringify(req.body));
+module.exports = (req, res) => { 
   res.send("hello");
-  console.log("\n", req.body);
-
-  // console.log(dd);
-  // const { fullName, amount, budget, org, purpose, email } = JSON.parse(
-  //   req.body
-  // );
-  // console.log(fullName, amount, org, budget, purpose, email);
+  console.log("\n", req.body); 
   return;
   if (req.method === "POST" || req.method === "OPTIONS") {
     const { fullName: name, email } = req.body;
